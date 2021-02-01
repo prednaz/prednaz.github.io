ba1607e77283bfe6b2b05f1412122ce56ba83afa
rednaZ <p.rednaz@googlemail.com>
2021-02-01 23:30:25 +0100

    "the professional" theme

--- site.hs
+++ site.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
+import 			 Control.Applicative
 
 
 --------------------------------------------------------------------------------
@@ -15,10 +16,10 @@ main = hakyllWith (defaultConfiguration {destinationDirectory = "docs"}) $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.md", "contact.md"]) $ do
+    match (fromList ["about.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" siteCtx
             >>= relativizeUrls
 
     match "posts/*" $ do
@@ -35,7 +36,7 @@ main = hakyllWith (defaultConfiguration {destinationDirectory = "docs"}) $ do
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Archives"            `mappend`
-                    defaultContext
+                    siteCtx
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
@@ -49,18 +50,30 @@ main = hakyllWith (defaultConfiguration {destinationDirectory = "docs"}) $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
                     listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+                    constField "title" "Home"                `mappend`
+                    siteCtx
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
 
-    match "templates/*" $ compile templateBodyCompiler
+    match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+    siteCtx
+
+siteCtx :: Context String
+siteCtx = 
+	activeClassField `mappend`
+	defaultContext
+
+-- https://groups.google.com/forum/#!searchin/hakyll/if$20class/hakyll/WGDYRa3Xg-w/nMJZ4KT8OZUJ 
+activeClassField :: Context a 
+activeClassField = functionField "activeClass" $ \[p] _ -> do 
+	path <- toFilePath <$> getUnderlying 
+	return $ if path == p then "active" else path 
